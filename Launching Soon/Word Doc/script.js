092cf8c0ef383c58e97c9055bc908dea770a43b9
Samuel-2552 <201501502@rajalakshmi.edu.in>
2022-08-30 21:24:30 +0530

    Word Docs Layout Ready

---
+++ Launching Soon/Word Doc/script.js
@@ -0,0 +1,17 @@
+function openLeftMenu() {
+    document.getElementById("leftMenu").style.width = "100px";
+  document.getElementById("leftMenu").style.display = "block";
+}
+
+function closeLeftMenu() {
+  document.getElementById("leftMenu").style.display = "none";
+}
+
+function openRightMenu() {
+    document.getElementById("rightMenu").style.width = "100px";
+  document.getElementById("rightMenu").style.display = "block";
+}
+
+function closeRightMenu() {
+  document.getElementById("rightMenu").style.display = "none";
+}
